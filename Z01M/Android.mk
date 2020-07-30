cffce87b5174bf71eb9184072b2d05347aa24839
Seplx <superseplex@protonmail.com>
2020-07-30 21:25:40 +0200

    remove asussplendid from vendor

--- Z01M/Android.mk
+++ Z01M/Android.mk
@@ -122,19 +122,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := AsusSplendid
-LOCAL_MODULE_OWNER := asus
-LOCAL_SRC_FILES := proprietary/priv-app/AsusSplendid/AsusSplendid.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PRIVILEGED_MODULE := true
-include $(BUILD_PREBUILT)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := FlipCover3
 LOCAL_MODULE_OWNER := asus
@@ -146,7 +133,6 @@ LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := .apk
 LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
-
 
 ifeq ($(QCPATH),)
 include $(CLEAR_VARS)
